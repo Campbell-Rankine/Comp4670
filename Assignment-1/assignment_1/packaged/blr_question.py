509a6b398379d5c1f0863298fd338a3ac5f2c014
Campbell Rankine <u6997593@anu.edu.au>
2022-03-24 20:08:45 +1100

    Question 3.6

--- Assignment-1/assignment_1/packaged/blr_question.py
+++ Assignment-1/assignment_1/packaged/blr_question.py
@@ -9,5 +9,18 @@ def single_EM_iter_blr(features, targets, alpha_i, beta_i):
     # and using that computes the new alpha and beta values.
     # Should return M_n, S_n, new_alpha, new_beta in that order, with return shapes (M,1), (M,M), None, None
     ### CODE HERE ###
-    raise NotImplementedError
+    fs = features.shape
+    ts = targets.shape
+    sni = alpha_i*np.eye(fs[1]) + beta_i*(features.T@features)
+    sn = np.linalg.inv(sni)
+    print(sn)
+    print(sn.shape)
+    mn = (beta_i*sn)@features.T@targets
+    Ed1 = mn.T@mn + np.trace(sni)
+    Ed2 = (targets-features @ mn).T @ (targets-features @ mn) + np.trace(features.T @ features @ sn)
+    
+    new_alpha = (fs[1] / Ed1)[0][0]
+    new_beta = (fs[0] / Ed2)[0][0]
+    
+    print(new_alpha, new_beta)
     return mn, sn, new_alpha, new_beta # (M,1), (M,M), None, None
